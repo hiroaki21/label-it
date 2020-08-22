422779308bed915e60584b07fffadbc2700ecf99
hiroaki21 <k.hi0201t.mkksk@gmail.com>
2020-08-22 16:42:12 +0900

    「次へ」付箋投稿　非同期通信

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params) 
+    post = Post.create(post_params) 
+    render json:{post:post} 
   end
 
   private
---
+++ app/javascript/label_post.js
@@ -0,0 +1,47 @@
+function label_post() {
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    const formData = new FormData(document.getElementById("form"));
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+      const it = XHR.response.post;
+      const list = document.getElementById("list");
+      const formText = document.getElementById("body");
+
+      const date = new Date(it.created_at)
+      const timeFormat = date.toLocaleString();
+      
+      const HTML = `
+      <div class="col-sm-3">
+        <div class="card border-primary mb-3" style="max-width: 18rem;" data-id=${it.id}>
+          <div class="card-header">${timeFormat}</div>
+          <div class="card-body text-dark">
+            <h5 class="card-title">${it.body}</h5>
+          </div>
+        </div>
+      </div>
+      `;
+      if (formText.value.match(/\S/g)){
+      list.insertAdjacentHTML("afterend", HTML);
+      formText.value = "";  
+      }
+
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+      } else {
+        return null;
+      }
+    };
+
+    XHR.onerror = function () {
+      alert("Request failed");
+    };
+
+    e.preventDefault();
+  })
+}
+
+window.addEventListener("load", label_post);
