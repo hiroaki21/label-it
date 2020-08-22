3b2d4d402ae7a9ee0f58eb446623a255e00ccfa5
hiroaki21 <k.hi0201t.mkksk@gmail.com>
2020-08-22 15:44:20 +0900

    postsテーブル作成

--- db/migrate/20200822062810_create_posts.rb
+++ db/migrate/20200822062810_create_posts.rb
@@ -1,7 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-
+      t.text        :body,      null: false
+      t.references  :user,      null: false,foreign_key: true
       t.timestamps
     end
   end
