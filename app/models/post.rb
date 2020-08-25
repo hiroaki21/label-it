b18ccf7b94413b14b3e8b60300572630ebe187b3
hiroaki21 <k.hi0201t.mkksk@gmail.com>
2020-08-25 20:32:10 +0900

    searchメソッド定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   validates :body,presence: true, length:{maximum:140}
 
   belongs_to :user
+
+  def self.search(search)
+    if search != ""
+      Post.where('body LIKE(?)', "%#{search}%").order(id: "DESC")
+    else
+      Post.all.order(id: "DESC")
+    end
+  end
 end
