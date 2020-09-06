9446961507cb6db50c65cbccf31d3b1c4fb0842d
hiroaki21 <k.hi0201t.mkksk@gmail.com>
2020-09-07 03:21:10 +0900

    favoritesコントローラー 作成

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,13 @@
+class FavoritesController < ApplicationController
+  def create
+    favorite = current_user.favorites.create(memo_id: params[:memo_id])
+    redirect_to memo_path(favorite.memo.id)
+    flash[:notice] = "--お気に入りに登録しました--"
+  end
+
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to memo_path(favorite.memo.id)
+    flash[:notice] = "--お気に入りを解除しました--"
+  end
+end
