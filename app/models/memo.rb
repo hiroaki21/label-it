102f7136dd42e34e81390daf284ab6085a6131a6
hiroaki21 <k.hi0201t.mkksk@gmail.com>
2020-09-25 16:06:24 +0900

    [訂正]アソシエーション修正

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -3,7 +3,6 @@ class Memo < ApplicationRecord
   has_many :posts, :through => :memo_posts
   belongs_to :user
   has_many   :favorites, dependent: :destroy
-  has_many   :favorite_users, :through => :favorites, source: :user
 
   validates :title,presence: true, length:{maximum:30}
   validates :desc, presence: true, length:{maximum:3000}
