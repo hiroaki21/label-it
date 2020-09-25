243df79b2c893507624f5f34423f9a81d5a5b43e
hiroaki21 <k.hi0201t.mkksk@gmail.com>
2020-09-25 13:51:31 +0900

    [訂正]アソシエーション修正

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,7 +1,6 @@
 class Memo < ApplicationRecord
   has_many :memo_posts, dependent: :destroy
   has_many :posts, :through => :memo_posts
-  accepts_nested_attributes_for :memo_posts, allow_destroy: true
   belongs_to :user
   has_many   :favorites, dependent: :destroy
   has_many   :favorite_users, :through => :favorites, source: :user
